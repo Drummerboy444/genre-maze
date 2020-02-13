bb5d5edd341381c4839a3798ec357af82ac58a08
mgallimore88 <mgallimore88@gmail.com>
2020-02-13 11:34:18 +0000

    corrects object syntax

--- sketch_manager.py
+++ sketch_manager.py
@@ -5,13 +5,13 @@ from audio.manager import Manager
 
 class SketchManager:
     def setup(self):
-        audio_manager = Manager()
-        Manager.authenticate
-        Manager.play_sample
+        self.audio_manager = Manager()
+        self.audio_manager.authenticate()
         size(400, 400)
 
     def draw(self):
         if mouse_x > 50:
+            self.audio_manager.play_sample()
             stroke(255)
             ellipse((10,10),20,20)
             play = True 
